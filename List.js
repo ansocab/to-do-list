133ddf521a0a119060daa6a900d890e5fe57d1c7
Ana Caballero <ana.caballero@gmx.de>
2020-10-22 16:54:28 +0200

    add number of tasks done + add new list button (not functional)

--- List.js
+++ List.js
@@ -3,7 +3,7 @@ class List {
         this.name = name;
         this.selector = selector;
         this.target = document.querySelector(this.selector);
-        this.items = [{name: "Pack camera", done: false}, {name: "Buy toothpaste", done: false}, {name: "Save money", done: false}];
+        this.items = [];
     }
 
     addItem(value) {
@@ -25,12 +25,15 @@ class List {
         let ul = document.createElement("ul");
         ul.classList.add("list-group", "taskList");
         this.items.forEach((item, index) => {
+            let checkboxAttribute;
+            item.done ? checkboxAttribute = "checked" : checkboxAttribute = ""
+
             let li = document.createElement("li");
             li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "mb-1", "mt-1", "pt-2", "pr-1", "pb-2");
             li.innerHTML = 
             `<span>
                 <span>
-                    <input type="checkbox" aria-label="Checkbox for following text input" class="checkbox"/>
+                    <input type="checkbox" aria-label="Checkbox for following text input" class="checkbox" id="c-${index}" ${checkboxAttribute}/>
                 </span>
                 <span class="pl-3 taskText" id="t-${index}">
                     ${item.name}
@@ -48,5 +51,14 @@ class List {
         })
         this.target.innerHTML = "";
         this.target.appendChild(ul);
+
+        let numberOfTasksDone = 0;
+        this.items.forEach((item) => {
+            if (item.done) {
+                numberOfTasksDone ++;
+            }
+        })
+       
+        this.target.parentElement.children[1].innerHTML = `Tasks done: ${numberOfTasksDone}/${this.items.length}`
     }
 }
